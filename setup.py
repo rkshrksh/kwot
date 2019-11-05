273775b4a2424de65cd5e673d7d86a0982f60c11
rkshrksh <aarkaygautam@gmail.com>
2019-11-05 07:58:09 +0530

    added github url

--- setup.py
+++ setup.py
@@ -5,19 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kwot",
-    version="0.0.1",
+    version="0.0.2",
     author="Rakesh Gautam",
     author_email="rkshrkshme@gmail.com",
     description="An awesome random quote generator",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/rkshrksh",
+    url="https://github.com/rkshrksh/kwot",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     python_requires='>=3.6',
     include_package_data=True,
     install_requires=[
